ff3d8d870e932fd86bae1d8c076db571f45312e1
Sanikireddy <61445255+Sanikireddy@users.noreply.github.com>
2020-02-25 10:08:49 +0530

    Add files via upload

---
+++ BoxDeno7.java
@@ -0,0 +1,28 @@
+class Box
+{
+ double width;
+ double height;
+ double depth;
+ Box()
+{
+ width=3;
+ height=4;
+ depth=2;
+}
+double volume()
+{
+return width*height*depth;
+}
+class BoxDeno7
+{
+public static void main(String args[])
+{
+ Box mybox1=new Box();
+ Box mybox2=new Box(3,6,9);
+ double vol;
+vol=mybox1.volume();
+System.out.println("volume is"+vol);
+ vol=mybox2.volume();
+System.out.println("volume is"+vol);
+}
+}
